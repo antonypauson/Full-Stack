7c56685166fd6cf15bf7bd4984dd2ee5726276dc
Antony <13antonysun@gmail.com>
2024-09-29 18:57:50 +0530

    2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,34 +2,66 @@ import { useEffect, useState } from "react"
 import axios from 'axios'
 
 const Country = ({country}) => {
-  return (
-    <div>
+  if (country.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  }
+  else if (country.length > 1) {
+    return (
+      <ul>
+        {country.map(n => 
+          <li>{n.name.common}</li>
+        )}
+      </ul>
+    )
+  }
+  else if (country.length === 1){
+    //country length is 1
+    const selectedCountry = country[0]
+    return (
+      <div>
+        <h2>{selectedCountry.name.official}</h2>
+        <p>Capital: {selectedCountry.capital}</p>
+        <p>Continent: {selectedCountry.continents[0]}</p>
+        Languages:
+        <ul>
+        {Object.values(selectedCountry.languages).map((l,i) => 
+          <li>{l}</li>
+        )}
+        </ul>
+        {selectedCountry.flag}
+        
+      </div>
       
-    </div>
-  )
+    )
+  }
+  else{
+    return null
+  }
+  
 }
 const App = () => {
 
-  const [value, setValue] = useState('bad')
+  const [value, setValue] = useState('')
   const [country, setCountry] = useState([])
+  const [status, setStatus] = useState("")
 
   useEffect(() => {
+    if (value) {
     axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
     .then(response => {
       const filteredCountries = response.data.filter(country => 
         country.name.official.toLowerCase().includes(value.toLowerCase())
       )
-      if (filteredCountries.length > 10) {
-        setCountry('Too many matches, specify another filter')
-      }
-      else if (filteredCountries.length === 1) {
-        setCountry('Just one country baby')
-      }
-      else {
-        setCountry(filteredCountries.map(country => country.name.official))
-      }
+      setCountry(filteredCountries)
+      setStatus("")
     })
+    }
+    else {
+      setCountry([])
+      setStatus("")
+    }
   },[value])
+
   const handleChange = (event) => {
     setValue(event.target.value)
   }
@@ -39,8 +71,9 @@ const App = () => {
       <form>
         find countries<input value={value} onChange={handleChange}/>
       </form>
+      {status}
       <div>
-        {JSON.stringify(country)}
+        {/* {JSON.stringify(country)} */}
         <Country country={country}/>
       </div>
     </div>
